4840aa0f942dce717d2e4b4d863a8967dbd82840
Michał Dziwisz <michal@dziwisz.net>
2026-02-21 16:57:34 +0100

    Tests: add app host for iOS unit tests

---
+++ Tests/AccessibilityTests/Host/Sources/AppDelegate.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+@objc(Application)
+public final class Application: UIApplication {
+}
+
+@objc(AppDelegate)
+public final class AppDelegate: NSObject, UIApplicationDelegate {
+    public var window: UIWindow?
+
+    public func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        let window = UIWindow()
+        self.window = window
+
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
+
