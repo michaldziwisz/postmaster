c744c93aef42a5b7ed7a652f4082a2d714a69919
Michał Dziwisz <michal@dziwisz.net>
2026-03-04 01:21:51 +0100

    Fix CI: disambiguate item provider file load

--- submodules/MediaPickerUI/Sources/MediaPickerScreen.swift
+++ submodules/MediaPickerUI/Sources/MediaPickerScreen.swift
@@ -1297,7 +1297,7 @@ public final class MediaPickerScreenImpl: ViewController, MediaPickerScreen, Att
 
                 if let movieType {
                     group.enter()
-                    provider.loadFileRepresentation(forTypeIdentifier: movieType) { url, _ in
+                    _ = provider.loadFileRepresentation(forTypeIdentifier: movieType) { (url: URL?, _: Error?) in
                         defer { group.leave() }
                         guard let url else {
                             return
