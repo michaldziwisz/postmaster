018f772e70c644c32614b81553c2fdf006afcc5b
Michał Dziwisz <michal@dziwisz.net>
2026-03-08 21:38:23 +0100

    Build sideload IPA without provisioning profiles

--- build-system/Make/Make.py
+++ build-system/Make/Make.py
@@ -654,6 +654,8 @@ def build(bazel, arguments):
     bazel_command_line.set_profile_swift(arguments.profileSwift)
     if getattr(arguments, "disableExtensions", False):
         bazel_command_line.set_disable_extensions()
+    if getattr(arguments, "disableProvisioningProfiles", False):
+        bazel_command_line.set_disable_provisioning_profiles()
 
     bazel_command_line.set_split_swiftmodules(arguments.enableParallelSwiftmoduleGeneration)
 
